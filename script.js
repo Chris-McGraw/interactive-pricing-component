4d6ff824010be8ca651ea6b6d0da2d3e07c74d6a
Chris McGraw <chris.rmcgraw1@gmail.com>
2021-03-17 15:34:13 -0400

    add price range slider

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ----------------------------- EVENT HANDLERS ----------------------------- */
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("price-slider").oninput = function() {
+    var value = (this.value - this.min) / (this.max - this.min) * 100;
+
+    this.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) "
+                            + value + "%, hsl(224, 65%, 95%) " + value + "%, hsl(224, 65%, 95%) 100%)";
+  };
+
+});
