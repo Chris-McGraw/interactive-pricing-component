e0e372726be304acb54fce5ecc6b273760027a5c
Chris McGraw <chris.rmcgraw1@gmail.com>
2021-03-18 11:12:09 -0400

    add pageviews-for-price-container/handleSliderChange() script

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
+/* ------------------------- FUNCTION DECLARATIONS ------------------------- */
+function handleSliderChange(value) {
+  let pageViews = document.getElementById("pageviews");
+  let price = document.getElementById("price");
+
+  switch(value) {
+    case "2":
+      pageViews.innerHTML = "1M Pageviews";
+      price.innerHTML = "$36.00<span id='price-per-span'> / month</span>";
+      break;
+    case "1":
+      pageViews.innerHTML = "500K Pageviews";
+      price.innerHTML = "$24.00<span id='price-per-span'> / month</span>";
+      break;
+    case "0":
+      pageViews.innerHTML = "100K Pageviews";
+      price.innerHTML = "$16.00<span id='price-per-span'> / month</span>";
+      break;
+    case "-1":
+      pageViews.innerHTML = "50K Pageviews";
+      price.innerHTML = "$12.00<span id='price-per-span'> / month</span>";
+      break;
+    case "-2":
+      pageViews.innerHTML = "10K Pageviews";
+      price.innerHTML = "$8.00<span id='price-per-span'> / month</span>";
+      break;
+    default:
+      break;
+  }
+}
+
+
+
 /* ----------------------------- EVENT HANDLERS ----------------------------- */
 document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("price-slider").oninput = function() {
-    var value = (this.value - this.min) / (this.max - this.min) * 100;
+    handleSliderChange(this.value);
+
+    var fillTrackPct = (this.value - this.min) / (this.max - this.min) * 100;
 
     this.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) "
-                            + value + "%, hsl(224, 65%, 95%) " + value + "%, hsl(224, 65%, 95%) 100%)";
+                            + fillTrackPct + "%, hsl(224, 65%, 95%) " + fillTrackPct + "%, hsl(224, 65%, 95%) 100%)";
   };
 
 });
