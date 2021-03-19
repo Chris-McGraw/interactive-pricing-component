2f9b781cac0004ca276b586af8b2a382abf5fe18
Chris McGraw <chris.rmcgraw1@gmail.com>
2021-03-19 11:39:27 -0400

    add billing-interval-container/getPriceForBillingInterval() script

--- script.js
+++ script.js
@@ -1,31 +1,72 @@
+/* ---------------------------- GLOBAL VARIABLES ---------------------------- */
+let ipcPrice = 16.00;
+let ipcPricingInterval = "month";
+
 /* ------------------------- FUNCTION DECLARATIONS ------------------------- */
+function getPriceForBillingInterval(price, int) {
+  if(int === "month") {
+    ipcPrice = price;
+  }
+  else {
+    ipcPrice = (price * 12) * 0.75;
+  }
+}
+
 function handleSliderChange(value) {
   let pageViews = document.getElementById("pageviews");
   let price = document.getElementById("price");
 
   switch(value) {
     case "2":
+      ipcPrice = 36;
+      getPriceForBillingInterval(ipcPrice, ipcPricingInterval);
+
       pageViews.innerHTML = "1M Pageviews";
-      price.innerHTML = "$36.00<span id='price-per-span'> / month</span>";
+      price.innerHTML = "$" + ipcPrice.toFixed(2) + "<span id='price-per-span'> / " + ipcPricingInterval + "</span>";
       break;
     case "1":
+      ipcPrice = 24;
+      getPriceForBillingInterval(ipcPrice, ipcPricingInterval);
+
       pageViews.innerHTML = "500K Pageviews";
-      price.innerHTML = "$24.00<span id='price-per-span'> / month</span>";
+      price.innerHTML = "$" + ipcPrice.toFixed(2) + "<span id='price-per-span'> / " + ipcPricingInterval + "</span>";
       break;
     case "0":
+      ipcPrice = 16;
+      getPriceForBillingInterval(ipcPrice, ipcPricingInterval);
+
       pageViews.innerHTML = "100K Pageviews";
-      price.innerHTML = "$16.00<span id='price-per-span'> / month</span>";
+      price.innerHTML = "$" + ipcPrice.toFixed(2) + "<span id='price-per-span'> / " + ipcPricingInterval + "</span>";
       break;
     case "-1":
+      ipcPrice = 12;
+      getPriceForBillingInterval(ipcPrice, ipcPricingInterval);
+
       pageViews.innerHTML = "50K Pageviews";
-      price.innerHTML = "$12.00<span id='price-per-span'> / month</span>";
+      price.innerHTML = "$" + ipcPrice.toFixed(2) + "<span id='price-per-span'> / " + ipcPricingInterval + "</span>";
       break;
     case "-2":
+      ipcPrice = 8;
+      getPriceForBillingInterval(ipcPrice, ipcPricingInterval);
+
       pageViews.innerHTML = "10K Pageviews";
-      price.innerHTML = "$8.00<span id='price-per-span'> / month</span>";
+      price.innerHTML = "$" + ipcPrice.toFixed(2) + "<span id='price-per-span'> / " + ipcPricingInterval + "</span>";
       break;
     default:
       break;
+  }
+}
+
+function handleBillingIntToggle(value) {
+  if(value === true) {
+    ipcPricingInterval = "year";
+
+    handleSliderChange(document.getElementById("price-slider").value);
+  }
+  else {
+    ipcPricingInterval = "month";
+
+    handleSliderChange(document.getElementById("price-slider").value);
   }
 }
 
@@ -41,6 +82,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     this.style.background = "linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) "
                             + fillTrackPct + "%, hsl(224, 65%, 95%) " + fillTrackPct + "%, hsl(224, 65%, 95%) 100%)";
+  };
+
+  document.getElementById("interval-switch").oninput = function() {
+    handleBillingIntToggle(this.checked);
   };
 
 });
